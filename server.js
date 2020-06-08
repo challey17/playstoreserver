d4f4ef0f368a35f5abb38645ae88c8f22c08e3ee
Chayce Knaub <chknaub@yahoo.com>
2020-06-07 22:27:18 -0400

    incomplete

--- server.js
+++ server.js
@@ -3,11 +3,36 @@ const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
+const storeApps = require("./playstore-data");
 
 app.use(morgan("common")); // let's see what 'common' format looks like
 
-app.get("/books", (req, res) => {
-  // ALL OUR CODE HERE
+app.get("/apps", (req, res) => {
+  const { genres, sort } = req.query;
+
+  if (sort) {
+    if (!["rating", "app"].includes(sort)) {
+      return res.status(400).send("Sort must be one of rating or app");
+    }
+  }
+
+  if (genres) {
+    if (!["Action", "Strategy", "Casual", "Arcade", "Card"].includes(genres)) {
+      return res.status(400).send("Genre must be a valid");
+    }
+  }
+
+  let results = storeApps.filter((app) =>
+    app.title.toLowerCase().includes(sort.toLowerCase())
+  );
+
+  if (sort) {
+    results.sort((a, b) => {
+      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    });
+  }
+
+  res.json(results);
 });
 
 app.listen(8000, () => {
